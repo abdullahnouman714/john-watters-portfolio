7c978aad9ebb91c86aee45b6c114722a9af92d71
Abdullah Nouman <abdullahnouman714@gmail.com>
2026-09-10 17:46:51 +0500

    Fix public path for john-watters.jpg in About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion'
-import johnPhoto from '../assets/john-watters.jpg'
+import johnPhoto from '/john-watters.jpg'
 
 export default function About() {
   return (
